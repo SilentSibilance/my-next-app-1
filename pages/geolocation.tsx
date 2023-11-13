91260b8565866943fa4c23a80fbbcbfe47ace283
SilentSibilance <tasha0coote@gmail.com>
2023-11-12 22:13:25 -0800

    Add geolocation refresh

--- pages/geolocation.tsx
+++ pages/geolocation.tsx
@@ -7,21 +7,33 @@ const GeolocationPage: React.FC = () => {
 
   useEffect(() => {
     // Check if geolocation is supported
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          });
-        },
-        (err) => {
-          setError(`Error getting location: ${err.message}`);
-        }
-      );
-    } else {
-      setError('Geolocation is not supported');
-    }
+    const getLocation = () => {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLocation({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+            setError(null);
+          },
+          (err) => {
+            setError(`Error getting location: ${err.message}`);
+          }
+        );
+      } else {
+        setError('Geolocation is not supported');
+      }
+    };
+
+    // Initial location fetch
+    getLocation();
+
+    // Refresh location every 5 seconds
+    const intervalId = setInterval(getLocation, 5000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
